579323b0e575c04bece78b447c62ed3bd2d4ce36
boustor <boustor.dev@gmail.com>
2021-02-26 13:29:26 +0100

    Pours cours

--- frontend/js/confirmation.js
+++ frontend/js/confirmation.js
@@ -2,9 +2,43 @@ function affichageConfirmation() {
     let panier = JSON.parse(localStorage.getItem("commande"));
     console.log(panier);
     document.getElementById('noCde').innerHTML = panier["orderId"];
-    const produit = panier['products']
-    console.log(produit);
-}
+    const contact = panier['contact'];
+    const produit = panier['products'];
 
+    console.log(produit);
+    affichageContact(contact);
+    affichageProduit(produit);
+}
+/* 
+    Affichage du contact 
+*/
+function affichageContact(contact) {
+    const adresse = `${contact.firstName} ${contact.lastName}
+                      <br>
+                     ${contact.adresse}
+                     <br>
+                     ${contact.city}
+                     <br>
+                     ${contact.email}
+                    `;
+    document.getElementById('adresse').innerHTML = adresse;
+}
+/*
+    Affichage des produits
+*/
+function affichageProduit(produit) {
+    let ligneProduit = '';
+    produit.array.forEach(ligne => {
+        ligneProduit += `
+        <tr>
+            <td>${ligne.name}</td>
+            <td>1<td>
+            <td>${ligne.price}</td>
+            <td>2</td>
+        </tr>
+        `
+    });
+    document.getElementById('ligneProduit').innerHTML = ligneProduit;
+}
 /* ***************************************************** */
 affichageConfirmation();
